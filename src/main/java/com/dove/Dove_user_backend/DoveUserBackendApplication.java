8cc94925f853346f7f26de8759fb84c8390e934d
이승윤 <yyuunn17@naver.com>
2021-01-19 18:16:56 +0900

    modify: delete @EnableAsync

--- src/main/java/com/dove/Dove_user_backend/DoveUserBackendApplication.java
+++ src/main/java/com/dove/Dove_user_backend/DoveUserBackendApplication.java
@@ -2,10 +2,8 @@ package com.dove.Dove_user_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class DoveUserBackendApplication {
 
 	public static void main(String[] args) {
