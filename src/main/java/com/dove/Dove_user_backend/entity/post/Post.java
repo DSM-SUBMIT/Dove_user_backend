ba11f40bcdbf4132f24220d2ea82ee470f85f504
이승윤 <yyuunn17@naver.com>
2021-01-29 23:22:16 +0900

    [ADD] 엔티티, 레포지토리 추가

---
+++ src/main/java/com/dove/Dove_user_backend/entity/post/Post.java
@@ -0,0 +1,43 @@
+package com.dove.Dove_user_backend.entity.post;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tbl_post")
+@Entity
+public class Post {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String host;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String writer;
+
+    private String description;
+
+    private String link;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate eventDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    private LocalDateTime createdAt;
+
+}
