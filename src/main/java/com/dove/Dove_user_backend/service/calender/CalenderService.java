1bab8fc92f940346ec01f75d2cee6b9c818278dc
이승윤 <yyuunn17@naver.com>
2021-01-29 21:39:18 +0900

    [DELETE] 캘린더 API

--- src/main/java/com/dove/Dove_user_backend/service/calender/CalenderService.java
+++
@@ -1,7 +0,0 @@
-package com.dove.Dove_user_backend.service.calender;
-
-import com.dove.Dove_user_backend.payload.response.EventListResponse;
-
-public interface CalenderService {
-    EventListResponse getCalender(Integer year, Integer month);
-}
