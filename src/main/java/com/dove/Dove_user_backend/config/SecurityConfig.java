2699ee223ac87b41d52f19e8da74a781b32d0ce6
이승윤 <yyuunn17@naver.com>
2021-01-31 16:32:35 +0900

    [UPDATE] cors

--- src/main/java/com/dove/Dove_user_backend/config/SecurityConfig.java
+++ src/main/java/com/dove/Dove_user_backend/config/SecurityConfig.java
@@ -2,30 +2,14 @@ package com.dove.Dove_user_backend.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .cors().and()
-                .sessionManagement().disable()
-                .formLogin().disable()
-                .authorizeRequests()
-                    .antMatchers(HttpMethod.GET, "/").permitAll()
-                    .antMatchers(HttpMethod.GET, "/post/**").permitAll()
-                    .antMatchers(HttpMethod.POST, "/post").permitAll();
-    }
+public class SecurityConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
